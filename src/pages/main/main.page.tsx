7f0087dac8fbe09b2b91ea3d2f68af2ddf214a4e
KirillArt <curillaenator@gmail.com>
2025-05-11 11:09:28 +0700

    fix: minor

--- src/pages/main/main.page.tsx
+++ src/pages/main/main.page.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
 const MainPage: React.FC = () => {
-  // const ad = import.meta.env.VITE_API_BASE_URL;
-
-  // console.log('MainPage', ad);
-
   return (
     <div data-mainpage>
       <h1>Hi there</h1>
